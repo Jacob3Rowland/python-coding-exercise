ee6730d1fb728e96adaa5699014cad595ea34098
Jacob <Jacob.3.Rowland@gmail.com>
2024-08-22 16:08:32 +1000

    Created splitter that splits into equal parts plus some remainder, added other test

--- src/coding_exercise/application/splitter.py
+++ src/coding_exercise/application/splitter.py
@@ -1,14 +1,16 @@
 from coding_exercise.domain.model.cable import Cable
-
+import math
 
 class Splitter:
 
-    def __validate(self):
-        valid = True
+    def __validate(self, valid = True):
         if not valid:
             raise ValueError
 
     def split(self, cable: Cable, times: int) -> list[Cable]:
-        self.__validate()
+        max_length = math.floor(cable.length / (times + 1))
+        self.__validate(max_length > 0)
+        remainder = cable.length % (times+1)
 
-        return []
+        return [Cable(max_length, "coconuts-" + "{:02d}".format(i)) for i in range(0,times + 1)] + \
+               [Cable(remainder, "coconuts-" + "{:02d}".format(times+1))]
--- tests/coding_exercise/application/test_splitter.py
+++ tests/coding_exercise/application/test_splitter.py
@@ -6,3 +6,9 @@ from coding_exercise.domain.model.cable import Cable
 
 def test_should_not_return_none_when_splitting_cable():
     assert_that(Splitter().split(Cable(10, "coconuts"), 1)).is_not_none()
+
+def test_should_return_expected_name_and_length_when_splitting_cable():
+    assert_that(Splitter().split(Cable(10, "coconuts"), 1)[0].length).is_equal_to(5)
+    assert_that(Splitter().split(Cable(10, "coconuts"), 1)[1].length).is_equal_to(5)
+    assert_that(Splitter().split(Cable(10, "coconuts"), 1)[0].name).is_equal_to("coconuts-00")
+    assert_that(Splitter().split(Cable(10, "coconuts"), 1)[1].name).is_equal_to("coconuts-01")
